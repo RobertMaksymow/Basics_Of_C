0a5e332f26eb3e3c20f118cd38f1695d348e415a
Dell <robert.maksymow@tutanota.com>
2026-09-05 21:40:59 +0100

    Array introduction

---
+++ 02_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+  int evens[] = {2, 4, 6, 8, 10, 12};
+  int odds[] = {1, 4, 5, 7, 10, 11}; // Do not modify this line.
+  
+  printf("%i\n", evens[5]); //12
+  printf("%i\n", evens[2]); //6
+  odds[1]=3;
+  odds[4]=9;
+
+    int arr[] = {3, 5, 7, 9}; // Array creation
+    arr[2] = 4; // Modify the third element
+    int x = arr[2]; // Assign the third element to the lone variable x
+    printf("%i", x); // Print x
+
+}
